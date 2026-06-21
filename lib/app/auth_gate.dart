d41a7f33189c61549ed801fa6bfb987db4452507
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-21 14:16:26 +0100

    Load saved session inside auth gate

--- lib/app/auth_gate.dart
+++ lib/app/auth_gate.dart
@@ -4,18 +4,39 @@ import '../core/auth/auth_session.dart';
 import '../features/auth/widgets/staff_login_screen.dart';
 import 'connected_admin_home.dart';
 
-class AuthGate extends StatelessWidget {
+class AuthGate extends StatefulWidget {
   const AuthGate({super.key});
 
   @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  late final Future<void> _loadFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFuture = AuthSession.instance.load();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: AuthSession.instance,
-      builder: (context, _) {
-        if (AuthSession.instance.isSignedIn) {
-          return const ConnectedAdminHome();
+    return FutureBuilder<void>(
+      future: _loadFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
         }
-        return const StaffLoginScreen();
+        return AnimatedBuilder(
+          animation: AuthSession.instance,
+          builder: (context, _) {
+            if (AuthSession.instance.isSignedIn) {
+              return const ConnectedAdminHome();
+            }
+            return const StaffLoginScreen();
+          },
+        );
       },
     );
   }
